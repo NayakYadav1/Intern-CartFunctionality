7c593c54abdf6db8e09aa4923563335c2409552a
NayakYadav1 <yadavnayak202@gmail.com>
2025-01-31 00:10:40 +0545

    Server created

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json())
+
+// app.use('/users', userRoutes);
+// app.use('/orders', orderRoutes);
+
+app.listen(port, () => {
+    console.log(`Server Successfully Started on ${port}`);
+})

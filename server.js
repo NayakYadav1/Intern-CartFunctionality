142dfb9e391d754eea84b07d07305ae437ba072c
NayakYadav1 <yadavnayak202@gmail.com>
2025-01-30 22:24:51 +0545

    Folder Structure

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
